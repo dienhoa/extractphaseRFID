f0cdcd13d1a24832e386400cdf1d3462bdcf0b43
U-LAPTOP-OH8ME6MO\dienh <dienhoa.t@gmail.com>
2017-05-02 12:23:25 +0200

    add command

--- calibrate.py
+++ calibrate.py
@@ -30,16 +30,13 @@ except socket.gaierror:
 s.connect((remote_ip , port))
  
 print 'Socket Connected to ' + host + ' on ip ' + remote_ip + ' at port ' + str(port)
+ #make socket non blocking
+s.setblocking(0)
  
- 
-def recv_timeout(the_socket,timeout=1):
-    #make socket non blocking
-    the_socket.setblocking(0)
-     
+def recv_timeout(timeout=1):
     #total data partwise in an array
     total_data=[];
-    data='';
-     
+    data=''; 
     #beginning time
     begin=time.time()
     while 1:
@@ -48,7 +45,7 @@ def recv_timeout(the_socket,timeout=1):
             break
         #recv something
         try:
-            data = the_socket.recv(8192)
+            data = s.recv(8192)
             if data:
                 total_data.append(data)
         except:
@@ -56,33 +53,41 @@ def recv_timeout(the_socket,timeout=1):
 
     return ''.join(total_data)
  
-recv_timeout(s) # To ignore the first input
+recv_timeout(0.1) # To ignore the first input
 
-raw_input('Put the tag to position of 1 antenna and Press enter to continue: ')
-info_calibrate = recv_timeout(s)
+raw_input('\nPut the tag to position of 1 antenna and Press enter to continue: ')
+recv_timeout(0.1)# to flush the previous data
+info_calibrate = recv_timeout(1.5)
 if info_calibrate:
-    phases_calib = []
+    phases_init = []
     print "calibrate information\n" + info_calibrate
     for line in info_calibrate.splitlines():
         EPC, temps, RSSI, count, ant, freq, phase = (item.strip() for item in line.split('\t'))
-        phases_calib.append(int(phase))  
-    print "phases_calib: " + str(phases_calib)
-    phase_calib = int(np.mean(phases_calib))
-    print "average of calibrated phase: " + str(phase_calib)
+        phases_init.append(int(phase))  
+    print "phases_init: " + str(phases_init)
+    phase_init = int(np.mean(phases_init))
+    print "average of calibrated phase: " + str(phase_init)
 
-raw_input('Put the tag to the position you want to calibrate')
+raw_input('\nPut the tag to the position you want to calibrate')
+
+phase_n_1 = phase_init
+phase_n  = phase_init
 
 while 1:
-    try:
+    command = raw_input(" \n- choose the command (r:read, e:exit) ")
+    if command == 'r':
         phases = []
-        input_file = recv_timeout(s)
+        recv_timeout(0.1)# to flush the previous data
+        input_file = recv_timeout(1.5)
         if input_file:
             print input_file
             for line in input_file.splitlines():
                 EPC, temps, RSSI, count, ant, freq, phase = (item.strip() for item in line.split('\t'))
-                phases.append(int(phase)-phase_calib)  
-            print "phases - phases_calib:  " + str(phases)
-    except KeyboardInterrupt:
+                phases.append(int(phase)-phase_init)
+            print "phase_initial:  " + str(phase_init)
+            print "phases - phases_initial:  " + str(phases)
+            print "average delta phase: " + str(int(np.mean(phases)))
+    if command == 'e':
         print "KeyboardInterrupt"   
         s.close()
         break
--- phasedifference.py
+++ phasedifference.py
@@ -29,16 +29,14 @@ except socket.gaierror:
 s.connect((remote_ip , port))
  
 print 'Socket Connected to ' + host + ' on ip ' + remote_ip + ' at port ' + str(port)
+
+#make socket non blocking
+s.setblocking(0)
  
- 
-def recv_timeout(the_socket,timeout=1):
-    #make socket non blocking
-    the_socket.setblocking(0)
-     
+def recv_timeout(timeout=1):
     #total data partwise in an array
     total_data=[];
     data='';
-     
     #beginning time
     begin=time.time()
     while 1:
@@ -47,7 +45,7 @@ def recv_timeout(the_socket,timeout=1):
             break
         #recv something
         try:
-            data = the_socket.recv(8192)
+            data = s.recv(8192)
             if data:
                 total_data.append(data)
         except:
@@ -55,14 +53,13 @@ def recv_timeout(the_socket,timeout=1):
 
     return ''.join(total_data)
  
-# EPCs, times, RSSIs, counts, ants, freqs, phases_1, phases_2 = ([] for i in range(8))
-recv_timeout(s) # To ignore the first input
+recv_timeout() # To ignore the first input
 while 1:
     try:
         phases_1 = []
         phases_2 = []
         delta_phase = []
-        input_file = recv_timeout(s)
+        input_file = recv_timeout(1)
         if input_file:
             print input_file
             for line in input_file.splitlines():
